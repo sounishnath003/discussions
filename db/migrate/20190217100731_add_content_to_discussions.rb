e702fd83734e8b970b7d41383706a1cd9b02092d
SOUNISH NATH <flock.sinasini@gmail.com>
2019-02-17 22:55:33 +0530

    #717 __Action_text install insead fraola

--- db/migrate/20190217100731_add_content_to_discussions.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToDiscussions < ActiveRecord::Migration[6.0]
-  def change
-    add_column :discussions, :content, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_100731) do
+ActiveRecord::Schema.define(version: 2019_02_17_093625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_02_17_100731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
