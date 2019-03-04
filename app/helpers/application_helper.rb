7cf2334e0113ae19bc9eec0c80b00ae1047f76ef
SOUNISH NATH <flock.sinasini@gmail.com>
2019-03-04 14:36:26 +0530

    #725 __Gravatar user image added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def format_date_discussion(date)
     date.strftime("%B %d, %Y")
   end
+
+# 50150083276269
+  def gravatar_for(email)
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, alt: email, class: "rounded-full"
+  end
 end
