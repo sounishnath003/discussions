f447dd8f7f9348752e0007d4caa07b61070414c4
sounishnath003 <flock.sinasini@gmail.com>
2019-03-08 18:16:01 +0530

    731.2 __model discussion + user code

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,7 +1,22 @@
 class Discussion < ApplicationRecord
+  has_many :tags, through: :taggings
+  has_many :taggings
+
   has_rich_text :content
   belongs_to :user
   has_one_attached :ref_photo
 
   has_many :likes, dependent: :destroy
+
+# code for adding taggings section
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(", ")
+  end
+
 end
