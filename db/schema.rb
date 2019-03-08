79d80395a0c6b58bf83fc6a606af1ee4fda036fd
sounishnath003 <flock.sinasini@gmail.com>
2019-03-08 17:30:31 +0530

    #731 __tags + Taggings and user combination database connection done :-)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_074748) do
+ActiveRecord::Schema.define(version: 2019_03_08_115720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,13 +63,22 @@ ActiveRecord::Schema.define(version: 2019_03_06_074748) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "taggings", force: :cascade do |t|
     t.bigint "discussion_id"
+    t.bigint "user_id"
+    t.bigint "tag_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["discussion_id"], name: "index_likes_on_discussion_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
+    t.index ["discussion_id"], name: "index_taggings_on_discussion_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["user_id"], name: "index_taggings_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,4 +101,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_074748) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "likes", "discussions"
   add_foreign_key "likes", "users"
+  add_foreign_key "taggings", "discussions"
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "users"
 end
